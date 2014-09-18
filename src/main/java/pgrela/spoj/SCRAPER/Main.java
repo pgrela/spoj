8759f1fdecbf86e217cfeb0a2792d71361d5e007
Patryk Grela <patryk.grela@payu.pl>
2014-09-18 19:07:33 +0200

    [SCRAPPER] SCRAPPER

--- src/main/java/pgrela/spoj/SCRAPER/Main.java
+++ src/main/java/pgrela/spoj/SCRAPER/Main.java
@@ -7,6 +7,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.Reader;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     protected BufferedReader inputStream;
@@ -45,45 +49,94 @@ public class Main {
         outputStream.close();
     }
 
-    private boolean isFurnitureTransportationPossible(Skyscraper skyscraper) {
-        if (liftsAreGoingToFloor(skyscraper.getElevators(), skyscraper.getStartFloor())) {
-
+    public boolean isFurnitureTransportationPossible(Skyscraper skyscraper) {
+        if (areStartingAndTargetPointOnTheSameFloor(skyscraper)) {
+            return true;
         }
-        for(Elevator firstElevator : skyscraper.getElevators()){
-            for(Elevator otherElevator : skyscraper.getElevators()){
-                if(firstElevator==otherElevator){
+        Elevator startingElevator = getElevatorThatGoesTo(skyscraper.getElevators(), skyscraper.getStartFloor());
+        if (startingElevator == null) {
+            return false;
+        }
+        Set<Elevator> elevatorsToBeCheckedOut = new TreeSet<Elevator>();
+        Set<Elevator> unreachedElevators = new TreeSet<Elevator>();
+        Collections.addAll(unreachedElevators, skyscraper.getElevators());
+
+        elevatorsToBeCheckedOut.add(startingElevator);
+        unreachedElevators.remove(startingElevator);
+
+        while (!elevatorsToBeCheckedOut.isEmpty()) {
+            Elevator currentlyChecked = elevatorsToBeCheckedOut.iterator().next();
+            if (currentlyChecked.canReachFloor(skyscraper.getTargetFloor())) {
+                return true;
+            }
+            elevatorsToBeCheckedOut.remove(currentlyChecked);
+            Iterator<Elevator> elevatorIterator = unreachedElevators.iterator();
+            while (elevatorIterator.hasNext()) {
+                Elevator elevator = elevatorIterator.next();
+
+                Long firstCommonFloor = firstCommonFloor(currentlyChecked, elevator);
+                if (firstCommonFloor == null) {
                     continue;
                 }
+
+                if (firstCommonFloor < skyscraper.getHeight()) {
+                    elevatorsToBeCheckedOut.add(elevator);
+                    elevatorIterator.remove();
+                }
             }
+            elevatorsToBeCheckedOut.remove(currentlyChecked);
         }
         return false;
     }
 
-    private boolean liftsAreGoingToFloor(Elevator[] elevators, int floor) {
+    private boolean areStartingAndTargetPointOnTheSameFloor(Skyscraper skyscraper) {
+        return skyscraper.startFloor == skyscraper.targetFloor;
+    }
+
+    public Long firstCommonFloor(Elevator firstElevator, Elevator otherElevator) {
+        long gcd = gcd(firstElevator.getFrequency(), otherElevator.getFrequency());
+        long floorDiff = firstElevator.getStartingFloor() - otherElevator.getStartingFloor();
+        if (floorDiff % gcd != 0) {
+            return null;
+        }
+
+        long otherElevatorFrequencyByGcd = otherElevator.getFrequency() / gcd;
+
+        long inv = multiplicativeInverseModulo(firstElevator.getFrequency() / gcd,
+                otherElevatorFrequencyByGcd) + otherElevatorFrequencyByGcd;
+        long x = (-floorDiff / gcd % otherElevatorFrequencyByGcd + otherElevatorFrequencyByGcd) * inv % otherElevatorFrequencyByGcd;
+
+        long firstElevatorCommonFloor = (firstElevator.getStartingFloor() / gcd + x * firstElevator.getFrequency() / gcd)
+                * gcd;
+        if (firstElevatorCommonFloor < otherElevator.getStartingFloor()) {
+            return (((firstElevatorCommonFloor - otherElevator.getStartingFloor()) / gcd / otherElevatorFrequencyByGcd)
+                    % (firstElevator.getFrequency() / gcd) + firstElevator.getFrequency() / gcd) % (firstElevator
+                    .getFrequency() / gcd) * otherElevator.getFrequency() + otherElevator.getStartingFloor();
+        }
+        return firstElevatorCommonFloor;
+    }
+
+    private Elevator getElevatorThatGoesTo(Elevator[] elevators, long floor) {
         for (Elevator elevator : elevators) {
-            if (floor >= elevator.getStartingFloor()) {
-                int floorsToTravel = floor - elevator.getStartingFloor();
-                if (floorsToTravel % elevator.getFrequency() == 0) {
-                    return true;
-                }
+            if (elevator.canReachFloor(floor)) {
+                return elevator;
             }
         }
-        return false;
+        return null;
     }
 
     private Skyscraper readSkyscraper() {
-        int height = readInt();
+        long height = readLong();
         int lifts = readInt();
-        int startFloor = readInt();
-        int targetFloor = readInt();
+        long startFloor = readLong();
+        long targetFloor = readLong();
         Elevator[] elevators = new Elevator[lifts];
         for (int i = 0; i < lifts; i++) {
-            int startingFloor = readInt();
-            int frequency = readInt();
+            long frequency = readLong();
+            long startingFloor = readLong();
             elevators[i] = new Elevator(startingFloor, frequency);
         }
-        Skyscraper skyscraper = new Skyscraper(height, startFloor, targetFloor, elevators);
-        return skyscraper;
+        return new Skyscraper(height, startFloor, targetFloor, elevators);
     }
 
     protected int readInt() {
@@ -106,37 +159,91 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
-    int k=0;
-    public int gcd(int a, int b) {
-        if(a==0){
-            return b;
+
+    protected long readLong() {
+        try {
+            int c;
+            do {
+                c = inputStream.read();
+            } while (!Character.isDigit(c));
+            long result = Character.digit(c, 10);
+            while (true) {
+                c = inputStream.read();
+                if (Character.isDigit(c)) {
+                    result = result * 10 + Character.digit(c, 10);
+                } else {
+                    break;
+                }
+            }
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        k+=b/a;
-        return gcd(b%a,a);
     }
 
-    private class Skyscraper {
-        private int height;
-        private int startFloor;
-        private int targetFloor;
+    public long gcd(long a, long b) {
+        return a == 0 ? b : gcd(b % a, a);
+    }
+
+    protected long multiplicativeInverseModulo(long number, long modulo) {
+        Matrix2x2 matrix2x2 = new Matrix2x2();
+        while (number != 0) {
+            long factor = modulo / number;
+            long oldModulo = modulo;
+            modulo = number;
+            number = oldModulo % number;
+            matrix2x2 = matrix2x2.multiply(new Matrix2x2(0, 1, 1, -factor));
+        }
+        return matrix2x2.a10;
+    }
+
+    static class Matrix2x2 {
+        public long a00, a01, a10, a11;
+
+        Matrix2x2(long a00, long a01, long a10, long a11) {
+            this.a00 = a00;
+            this.a01 = a01;
+            this.a10 = a10;
+            this.a11 = a11;
+        }
+
+        Matrix2x2() {
+            a00 = a11 = 1;
+            a01 = a10 = 0;
+        }
+
+        Matrix2x2 multiply(Matrix2x2 otherMatrix) {
+            return new Matrix2x2(
+                    a00 * otherMatrix.a00 + a01 * otherMatrix.a10,
+                    a00 * otherMatrix.a01 + a01 * otherMatrix.a11,
+                    a10 * otherMatrix.a00 + a11 * otherMatrix.a10,
+                    a10 * otherMatrix.a01 + a11 * otherMatrix.a11
+            );
+        }
+    }
+
+    public static class Skyscraper {
+        private long height;
+        private long startFloor;
+        private long targetFloor;
         private Elevator[] elevators;
 
-        private Skyscraper(int height, int startFloor, int targetFloor, Elevator[] elevators) {
+        public Skyscraper(long height, long startFloor, long targetFloor, Elevator[] elevators) {
             this.height = height;
             this.startFloor = startFloor;
             this.targetFloor = targetFloor;
             this.elevators = elevators;
         }
 
-        public int getHeight() {
+        public long getHeight() {
             return height;
         }
 
-        public int getStartFloor() {
+        public long getStartFloor() {
             return startFloor;
         }
 
-        public int getTargetFloor() {
+        public long getTargetFloor() {
             return targetFloor;
         }
 
@@ -145,21 +252,40 @@ public class Main {
         }
     }
 
-    private class Elevator {
-        private int startingFloor;
-        private int frequency;
+    public static class Elevator implements Comparable {
+        private long startingFloor;
+        private long frequency;
 
-        private Elevator(int startingFloor, int frequency) {
+        public Elevator(long startingFloor, long frequency) {
             this.startingFloor = startingFloor;
             this.frequency = frequency;
         }
 
-        public int getStartingFloor() {
+        public Elevator(int startingFloor, int frequency) {
+            this.startingFloor = startingFloor;
+            this.frequency = frequency;
+        }
+
+        public long getStartingFloor() {
             return startingFloor;
         }
 
-        public int getFrequency() {
+        public long getFrequency() {
             return frequency;
+        }
+
+        public boolean canReachFloor(long targetFloor) {
+            return targetFloor >= startingFloor && (targetFloor - startingFloor) % frequency == 0;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Elevator elevator = (Elevator) o;
+            int i = Long.valueOf(startingFloor).compareTo(elevator.getStartingFloor());
+            if (i == 0) {
+                return Long.valueOf(frequency).compareTo(elevator.getFrequency());
+            }
+            return i;
         }
     }
 }
--- src/test/java/pgrela/spoj/SCRAPER/SCRAPERTest.java
+++ src/test/java/pgrela/spoj/SCRAPER/SCRAPERTest.java
@@ -1,20 +1,46 @@
 package pgrela.spoj.SCRAPER;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import pgrela.spoj.common.AbstractMainTest;
+import static junitparams.JUnitParamsRunner.$;
+import static org.assertj.core.api.BDDAssertions.then;
+import static pgrela.spoj.SCRAPER.Main.Elevator;
+import static pgrela.spoj.SCRAPER.SCRAPERTest.ElevatorBuilder.builder;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringReader;
 
-import static junitparams.JUnitParamsRunner.$;
-import static org.assertj.core.api.BDDAssertions.then;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import pgrela.spoj.common.AbstractMainTest;
 
 @RunWith(JUnitParamsRunner.class)
 public class SCRAPERTest extends AbstractMainTest {
+
+    @Test
+    @Parameters(
+            {
+                    "2\n" +
+                            "22 4 0 6\n" +
+                            "3 2\n" +           //2,5,8,11,14,17,20
+                            "4 7\n" +           //7,11,15,19
+                            "13 6\n" +          //6,19
+                            "10 0\n" +          //0,10,20
+                            "1000 2 500 777\n" +
+                            "2 0\n" +
+                            "2 1,It is possible to move the furniture.\n" +
+                            "The furniture cannot be moved.\n"
+            }
+    )
+    public void shouldWorkWithSampleInput(String input, String expectedOutput) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        mainClassFactory.getMain(Main.class, new StringReader(input), output).solve();
+
+        then(output.toString()).isEqualTo(expectedOutput);
+    }
 
     @Test
     @Parameters(
@@ -23,8 +49,164 @@ public class SCRAPERTest extends AbstractMainTest {
             }
     )
     public void shouldComputeGCD(int a, int b, int expectedGcd) throws IOException {
-        int gcd = mainClassFactory.getMain(Main.class).gcd(a, b);
+
+        //when
+        long gcd = mainClassFactory.getMain(Main.class).gcd(a, b);
 
         then(gcd).isEqualTo(expectedGcd);
+    }
+
+    @Test
+    @Parameters(
+            {
+                    "3,11",
+                    "1,7",
+                    "7,17",
+                    "3,4",
+                    "2,3"
+            }
+    )
+    public void shouldComputeInverse(int number, int modulo) throws IOException {
+        long inverseModulo = mainClassFactory.getMain(Main.class).multiplicativeInverseModulo(number, modulo);
+        System.out.println(inverseModulo);
+        then((inverseModulo * number + modulo * modulo) % modulo).isEqualTo(1);
+    }
+
+    public Object[] minimalTestCases() {
+        return $(
+                $(new Elevator(1L, 7L), new Elevator(100L, 17L), 134L),
+                $(new Elevator(2, 14), new Elevator(200, 34), 268L),
+                $(new Elevator(100, 17), new Elevator(1, 7), 134L),
+                $(new Elevator(200, 34), new Elevator(2, 14), 268L),
+                $(new Elevator(1, 2), new Elevator(2, 2), null),
+                $(new Elevator(0, 1), new Elevator(0, 2), 0L),
+                $(new Elevator(3, 8), new Elevator(3, 6), 3L),
+                $(new Elevator(1, 1), new Elevator(0, 2), 2L),
+                $(new Elevator(0, 2), new Elevator(1, 3), 4L),
+                $(new Elevator(0, 3), new Elevator(1, 4), 9L),
+                $(new Elevator(0, 6), new Elevator(2, 8), 18L)
+        );
+    }
+
+    @Test
+    @Parameters(method = "minimalTestCases")
+    public void shouldWorkForTestCase(Elevator firstElevator, Elevator otherElevator,
+                                      Long expectedCommonFloor) throws IOException {
+        //given
+        Main scraperSolver = mainClassFactory.getMain(Main.class);
+
+        //when
+        Long commonFloor = scraperSolver.firstCommonFloor(firstElevator, otherElevator);
+
+        then(commonFloor).isEqualTo(expectedCommonFloor);
+    }
+
+    public Object[] testCases() {
+        return $(
+//                $(SkyscraperBuilder.builder()
+//                                .withHeight(100)
+//                                .withStart(0)
+//                                .withTarget(1)
+//                                .withElevators(
+//                                        builder().withStart(0).withFrequency(3).build(),
+//                                        builder().withStart(1).withFrequency(97).build()
+//                                )
+//                                .build()
+//                        , false
+//                ),
+//                $(SkyscraperBuilder.builder()
+//                                .withHeight(100)
+//                                .withStart(0)
+//                                .withTarget(2)
+//                                .withElevators(
+//                                        builder().withStart(100).withFrequency(2).build(),
+//                                        builder().withStart(0).withFrequency(8).build(),
+//                                        builder().withStart(2).withFrequency(29).build()
+//                                )
+//                                .build()
+//                        , false
+//                ),
+                $(SkyscraperBuilder.builder()
+                                .withHeight(100)
+                                .withStart(0)
+                                .withTarget(1)
+                                .withElevators(
+                                        builder().withStart(0).withFrequency(1).build()
+                                )
+                                .build()
+                        , true
+                )
+        );
+    }
+
+    @Test
+    @Parameters(method = "testCases")
+    public void shouldWorkForTestCaseSky(Main.Skyscraper skyscraper, boolean isExpectedToBePossible) throws IOException {
+        //given
+        Main scraperSolver = mainClassFactory.getMain(Main.class);
+
+        //when
+        boolean isMovingPossible = scraperSolver.isFurnitureTransportationPossible(skyscraper);
+
+        then(isMovingPossible).isEqualTo(isExpectedToBePossible);
+    }
+
+    static class SkyscraperBuilder {
+        int height;
+        int start;
+        int target;
+        Elevator[] elevators;
+
+        public static SkyscraperBuilder builder() {
+            return new SkyscraperBuilder();
+        }
+
+        public SkyscraperBuilder withHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public SkyscraperBuilder withStart(int start) {
+            this.start = start;
+            return this;
+        }
+
+        public SkyscraperBuilder withTarget(int target) {
+            this.target = target;
+            return this;
+        }
+
+        public SkyscraperBuilder withElevators(Elevator... elevators) {
+            this.elevators = elevators;
+            return this;
+        }
+
+        Main.Skyscraper build() {
+            return new Main.Skyscraper(height, start, target, elevators);
+        }
+    }
+
+    public static class ElevatorBuilder {
+        int start;
+
+        public static ElevatorBuilder builder() {
+            return new ElevatorBuilder();
+        }
+
+        public ElevatorBuilder withFrequency(int frequency) {
+            this.frequency = frequency;
+            return this;
+        }
+
+        public ElevatorBuilder withStart(int start) {
+            this.start = start;
+            return this;
+        }
+
+        public Elevator build() {
+            return new Elevator(start, frequency);
+        }
+
+        int frequency;
     }
 }
